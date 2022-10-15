41cdd8c4d3b9888caf2cf981069c98a660a56a25
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-10-15 13:10:43 -0300

    feat: configuring styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+
+import { defaultTheme } from './styles/themes/default'
+
+import { GlobalStyle } from './styles/global'
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,37 @@
+export const defaultTheme = {
+  'white-100': '#F1F1F1',
+  'white-200': '#F6F6F6',
+  'white-300': '#FFFFFF',
+
+  'black-100': '#BDBDBD',
+  'black-200': '#828282',
+  'black-300': '#333333',
+  'black-400': '#282828',
+
+  'yellow-100': '#F1E27D',
+  'yellow-200': '#F3D76A',
+  'yellow-300': '#F8D930',
+  'yellow-400': '#EACB22',
+  'yellow-500': '#CDB517',
+
+  'light-green-100': '#C8FFED',
+  'light-green-200': '#8CFDD7',
+  'light-green-300': '#44F9BD',
+  'light-green-400': '#2DFCB7',
+  'light-green-500': '#21E0A1',
+
+  'blue-100': '#B7D5F8',
+  'blue-200': '#77B6FF',
+  'blue-300': '#56A4FF',
+  'blue-400': '#1D84FD',
+
+  'green-100': '#7dffed',
+  'green-200': '#72e6f0',
+  'green-300': '#69d4de',
+  'green-400': '#60c2cb',
+  'green-500': '#58b1b9',
+
+  'red-100': '#FF5A5A',
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+}
